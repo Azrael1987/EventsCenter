5b67cb0c429d56e673c6bde303ab45fbdcb8eb0c
Adam <gruszecki.adam@gmail.com>
2019-10-09 23:24:10 +0200

    Add files via upload

---
+++ Evento.Api/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using Evento.Infrastructure.Commands.Users;
+using Evento.Infrastructure.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Evento.Api.Controllers
+{
+    [Route("[controller]")]
+    public class AccountController : ApiControllerBase
+    {
+        private IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        /// <summary>
+        /// 
+        /// przykład:
+        ///     GET Account
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpGet]
+        // [AllowAnonymous]
+        [Authorize]
+        public async Task<IActionResult> Get() => Json(await _userService.GetAccountAsync(UserId));
+
+        [HttpGet("tickets")]
+        public async Task<IActionResult> GetTickets()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// przykład:
+        ///     POST Account/registry?email=logan@wp.pl&password=secret12345!@#
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPost("registry")]
+        public async Task<IActionResult> Post([FromBody]Register command)
+        {
+            await _userService.RegistryAsync(Guid.NewGuid(), command.Email, command.Name, command.Password, command.Role);
+            return Created("/account", null);
+        }
+
+        /// <summary>
+        /// 
+        /// przykład:
+        ///     POST Account/login?email=logan@wp.pl&password=secret12345!@#
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPost("login")]
+        public async Task<IActionResult> Post([FromBody]Login command)
+            => Json(await _userService.LoginAsync(command.Email, command.Password));
+    }
+}
---
+++ Evento.Api/Controllers/EventsController.cs
@@ -0,0 +1,91 @@
+using Evento.Infrastructure.Commands.Events;
+using Evento.Infrastructure.Services;
+//using Evento.Interface.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Evento.Api.Controllers
+{
+    [Route("[controller]")]
+    public class EventsController : ApiControllerBase
+    {
+        private readonly IEventService _eventService;
+        public EventsController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+        /// <summary>
+        /// 
+        /// przykład:
+        /// Get events
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> Get(string name)
+        {
+            var events = await _eventService.BrowseAsync(name);
+            return Json(events);
+        }
+
+        /// <summary>
+        /// 
+        /// przykład:
+        /// Get events/8c7fccff-0b70-4ee2-856d-48a978b721e9
+        /// </summary>
+        /// <param name="eventId"></param>
+        /// <returns> Szczegóły wydarzenia </returns>
+
+        [HttpGet("{eventId}")]
+        public async Task<IActionResult> GetDetails(Guid eventId)
+    {
+            var @event = await _eventService.GetAsync(eventId);
+            if(@event == null)
+            {
+                return NotFound(); // 404
+            }
+
+            return Json(@event);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]CreateEvent command)
+        {
+            command.EventId = Guid.NewGuid();
+            await _eventService.CreateAsync(command.EventId, command.Name, command.Description, command.StartDate, command.EndDate);
+            await _eventService.AddTicketsAsync(command.EventId, command.TicketsAmount, command.Price);
+            return Created($"/events/{command.EventId}", null); // 201
+        }
+
+        /// <summary>
+        ///  /events/{id} -> HTTP PUT
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPut("{eventId}")]
+        public async Task<IActionResult> Put(Guid eventId, [FromBody]UpdateEvent command)
+        {
+            await _eventService.UpdateAsync(eventId, command.Name, command.Description);
+
+            return NoContent(); // 204
+        }
+
+        /// <summary>
+        ///  /events/{id}
+        /// </summary>
+        /// <param name="eventId"></param>
+        /// <returns></returns>
+        [HttpDelete("{eventId}")]
+        public async Task<IActionResult> Delete(Guid eventId)
+        {
+            await _eventService.DeleteAsync(eventId);
+            return NoContent();
+        }
+        //filmik z rodziału 5.11) zabezpieczneie dostepu z atrybutem Authorize
+
+        // narzedzia do testowania URL - fiddler, postman, curl, SoupUi
+
+        // localhost:5001/events -X POST -H "Content-Type: application/json" -d '{"name":"moje wydarzenie","description":"opis wydarzenia","startDate":"2019-12-05","endDate":"2019-12-05","tickets":10,"price":100}'
+    }
+}
